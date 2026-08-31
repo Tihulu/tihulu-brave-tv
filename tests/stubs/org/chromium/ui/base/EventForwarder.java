525da50d998e571ef6ebd7f8d6fddcf89137a09b
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 16:13:35 +0200

    Stub Chromium EventForwarder TV bridge

---
+++ tests/stubs/org/chromium/ui/base/EventForwarder.java
@@ -0,0 +1,9 @@
+package org.chromium.ui.base;
+
+import android.view.MotionEvent;
+
+public class EventForwarder {
+    public boolean sendTihuluSyntheticMouseButtonEvent(MotionEvent event, int changedButton) {
+        return true;
+    }
+}
