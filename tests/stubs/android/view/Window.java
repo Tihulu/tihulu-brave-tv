4994ab4a6584d8f3f25ef23b2db24f6e3ade0518
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 14:39:14 +0200

    Extend Window test stub

--- tests/stubs/android/view/Window.java
+++ tests/stubs/android/view/Window.java
@@ -1,1 +1,8 @@
-package android.view; public class Window { public View getDecorView(){return new View(null);} public void setLayout(int w,int h){} }
+package android.view;
+
+public class Window {
+    public View getDecorView() { return new View(null); }
+    public void setLayout(int w, int h) {}
+    public void setGravity(int gravity) {}
+    public void setDimAmount(float amount) {}
+}
