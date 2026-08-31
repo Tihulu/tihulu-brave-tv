d00c69b47d9af97136869065363ef8b5a14bae47
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 13:58:34 +0200

    Stub Chromium network adapter for Java surface tests

---
+++ tests/stubs/org/chromium/net/ChromiumNetworkAdapter.java
@@ -0,0 +1,12 @@
+package org.chromium.net;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ChromiumNetworkAdapter {
+    public static URLConnection openConnection(URL url, NetworkTrafficAnnotationTag tag)
+            throws IOException {
+        return null;
+    }
+}
