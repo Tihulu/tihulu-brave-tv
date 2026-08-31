05b24ef460348f9edfa5508bb52220131a99bab4
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 13:50:42 +0200

    Expose TV cursor overlay to Brave onboarding

--- overlay/brave/android/java/org/chromium/chrome/browser/tv/TvCursorOverlay.java
+++ overlay/brave/android/java/org/chromium/chrome/browser/tv/TvCursorOverlay.java
@@ -12,11 +12,11 @@ import android.graphics.Paint;
 import android.view.View;
 
 /** Non-interactive view drawn inside a ViewOverlay to visualize the synthetic TV pointer. */
-final class TvCursorOverlay extends View {
+public final class TvCursorOverlay extends View {
     private final Paint mOuter = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Paint mInner = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    TvCursorOverlay(Context context) {
+    public TvCursorOverlay(Context context) {
         super(context);
         setFocusable(false);
         setClickable(false);
