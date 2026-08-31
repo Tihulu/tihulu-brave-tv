d9de33272c9a59c96fa6167735739b738d9e236a
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 12:03:05 +0200

    Model Chromium final onCreate lifecycle in Java stub

--- tests/stubs/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ tests/stubs/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1,1 +1,33 @@
-package org.chromium.chrome.browser; import android.content.Context; import android.os.Bundle; import android.view.KeyEvent; import android.view.View; import android.view.ViewGroup; import android.view.Window; public class ChromeTabbedActivity extends Context { protected void onCreate(Bundle b){} public boolean dispatchKeyEvent(KeyEvent e){return false;} public void addContentView(View v, ViewGroup.LayoutParams p){} public Window getWindow(){return new Window(){ @Override public android.view.View getDecorView(){return new ViewGroup(ChromeTabbedActivity.this);} };} }
+package org.chromium.chrome.browser;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+public class ChromeTabbedActivity extends Context {
+    protected final void onCreate(Bundle b) {}
+
+    public void performPostInflationStartup() {}
+
+    public boolean isFinishing() {
+        return false;
+    }
+
+    public boolean dispatchKeyEvent(KeyEvent e) {
+        return false;
+    }
+
+    public void addContentView(View v, ViewGroup.LayoutParams p) {}
+
+    public Window getWindow() {
+        return new Window() {
+            @Override
+            public android.view.View getDecorView() {
+                return new ViewGroup(ChromeTabbedActivity.this);
+            }
+        };
+    }
+}
