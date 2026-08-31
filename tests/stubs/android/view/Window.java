1e448108db12f04033473252b09b419f051cbda1
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 17:45:52 +0200

    Stub passive cursor window configuration

--- tests/stubs/android/view/Window.java
+++ tests/stubs/android/view/Window.java
@@ -1,8 +1,11 @@
 package android.view;
 
 public class Window {
+    public static final int FEATURE_NO_TITLE = 1;
     public View getDecorView() { return new View(null); }
     public void setLayout(int w, int h) {}
     public void setGravity(int gravity) {}
     public void setDimAmount(float amount) {}
+    public void addFlags(int flags) {}
+    public void clearFlags(int flags) {}
 }
