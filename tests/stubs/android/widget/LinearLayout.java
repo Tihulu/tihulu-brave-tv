609f0d77840e991fa315799559d036ab780df6a5
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 14:39:51 +0200

    Model weighted TV bar layout params

--- tests/stubs/android/widget/LinearLayout.java
+++ tests/stubs/android/widget/LinearLayout.java
@@ -1,1 +1,17 @@
-package android.widget; import android.content.Context; import android.view.View; import android.view.ViewGroup; public class LinearLayout extends ViewGroup { public static final int VERTICAL=1; public LinearLayout(Context c){super(c);} public void setOrientation(int o){} public void setPadding(int a,int b,int c,int d){} public void setBackgroundColor(int c){} public void addView(View v, LayoutParams p){} public static class LayoutParams extends ViewGroup.LayoutParams { public LayoutParams(int w,int h){super(w,h);} } }
+package android.widget;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+public class LinearLayout extends ViewGroup {
+    public static final int VERTICAL=1;
+    public LinearLayout(Context c){super(c);}
+    public void setOrientation(int o){}
+    public void setPadding(int a,int b,int c,int d){}
+    public void setBackgroundColor(int c){}
+    public void addView(View v, LayoutParams p){}
+    public static class LayoutParams extends ViewGroup.LayoutParams {
+        public float weight;
+        public LayoutParams(int w,int h){super(w,h);}
+        public LayoutParams(int w,int h,float weight){super(w,h);this.weight=weight;}
+    }
+}
