d1c0ebb034f3563a62c658b5c2ffb34abf36ca61
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 14:38:53 +0200

    Update dialog stub for TV bar tests

--- tests/stubs/android/app/Dialog.java
+++ tests/stubs/android/app/Dialog.java
@@ -1,1 +1,17 @@
-package android.app; import android.content.Context; import android.content.DialogInterface; import android.view.View; import android.view.Window; public class Dialog implements DialogInterface { public Dialog(Context c){} public void setContentView(View v){} public Window getWindow(){return new Window();} public void setOnShowListener(DialogInterface.OnShowListener l){} public void show(){} public void dismiss(){} }
+package android.app;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.Window;
+public class Dialog implements DialogInterface {
+    private boolean showing;
+    public Dialog(Context c) {}
+    public void setContentView(View v) {}
+    public Window getWindow() { return new Window(); }
+    public void setOnShowListener(DialogInterface.OnShowListener l) {}
+    public void setOnDismissListener(DialogInterface.OnDismissListener l) {}
+    public void setOnKeyListener(DialogInterface.OnKeyListener l) {}
+    public void show() { showing = true; }
+    public void dismiss() { showing = false; }
+    public boolean isShowing() { return showing; }
+}
