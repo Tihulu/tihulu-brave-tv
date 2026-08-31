451ec8c29b2b23a8c12f672210485d13bdacdbb6
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-31 17:58:32 +0200

    Add cursor window to Brave Java GN inventory

--- scripts/apply_overlay.py
+++ scripts/apply_overlay.py
@@ -21,6 +21,7 @@ JAVA_CLASSES = [
     "TvNavigationMode.java",
     "TvCursorState.java",
     "TvCursorOverlay.java",
+    "TvCursorWindow.java",
     "TvMouseDispatcher.java",
     "TvMemoryProfile.java",
     "TvControlPanel.java",
