8cd2d0b46936bf9d55f7766cf0870fc6e877c208
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-09-25 22:49:56 +0300

    lite: bump YouTube ad guard build to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.tihulu.tvlite"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     buildTypes {
