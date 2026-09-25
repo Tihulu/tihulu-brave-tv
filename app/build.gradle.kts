ae946b080e7f5d79f8a844849d612bb326cebd4c
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-09-25 22:57:55 +0300

    brave-adblock: mark experimental 0.3.0 build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.tihulu.tvlite"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.3.0-brave-adblock"
     }
 
     buildTypes {
